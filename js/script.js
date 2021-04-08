656d07a4fc174d0881cd3bf53db471cd60e9f36b
Andrés David Monroy Sanabria <Amonroy@intergrupo.com>
2021-04-08 12:23:30 -0500

    menu

--- js/script.js
+++ js/script.js
@@ -62,4 +62,106 @@ window.onload = function() {
     css.type = "text/css";
     css.innerHTML = ".txt-rotate > .wrap { border-right: .15em solid orange; }";
     document.body.appendChild(css);
+
+    var start = 1 - Math.round(Math.random()) * 2;
+    var scale = Math.random() * Math.random() * 0.8 + 0.2;
+    var bound = 30 + Math.random() * 20;
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
+    generateHeart(100, 500, bound, start, scale, 'stars');
+    generateHeart(150, 550, bound, start, scale, 'stars2');
+    generateHeart(200, 580, bound, start, scale, 'stars3');
+
+    generateHeart(1100, 580, bound, start, scale, 'stars4');
+    generateHeart(1150, 550, bound, start, scale, 'stars5');
+    generateHeart(1200, 500, bound, start, scale, 'stars6');
 };
+
+
+
+function generateHeart(x, y, xBound, xStart, scale, clase) {
+    var heart = document.createElement("DIV");
+    heart.setAttribute('class', clase);
+    brd.appendChild(heart);
+    heart.time = duration;
+    heart.x = x;
+    heart.y = y;
+    heart.bound = xBound;
+    heart.direction = xStart;
+    heart.style.left = heart.x + "px";
+    heart.style.top = heart.y + "px";
+    heart.scale = scale;
+    heart.style.transform = "scale(" + scale + "," + scale + ")";
+    if (hearts == null)
+        hearts = [];
+    hearts.push(heart);
+    return heart;
+}
